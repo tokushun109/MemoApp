026be85294f92ecda60b08c8fb6c45abee40639f
tokushun109 <tokushun109@gmail.com>
2024-02-07 10:13:04 +0900

    eslintの設定

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    )
+const App = (): React.JSX.Element => {
+  const [text, setText] = useState<string>('stateのテキストです')
+
+  return (
+    <View style={styles.container}>
+      <Text>{text}</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
+
+export default App
