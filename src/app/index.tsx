5f3106f8bb6c4f816e74b5e032af8faeb011f819
tokushun109 <tokushun109@gmail.com>
2024-02-07 14:09:07 +0900

    indexページを簡易作成

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,45 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): React.JSX.Element => {
+  // 空オブジェクトのリストを作成
+  const list: object[] = []
+  for (let index = 0; index < 10; index++) {
+    list.push({})
+  }
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      {list.map((_, i) => (
+        <View key={i}>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>{new Date().toDateString()}</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      ))}
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Index
