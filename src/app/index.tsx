6dacf654010ac74b364fd8ae39ccaef4dd0dfb9c
tokushun109 <tokushun109@gmail.com>
2024-02-07 16:01:47 +0900

    初期のリダイレクト処理を追加

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,30 +1,8 @@
-import { View, StyleSheet } from 'react-native'
-import Header from '../components/Header'
-import MemoListItem from '../components/MemoListItem'
-import CircleButton from '../components/CircleButton'
+import { Redirect } from 'expo-router'
 
 const Index = (): JSX.Element => {
-  // 空オブジェクトのリストを作成
-  const list: object[] = []
-  for (let index = 0; index < 2; index++) {
-    list.push({})
-  }
-  return (
-    <View style={styles.container}>
-      <Header />
-      {list.map((_, i) => (
-        <MemoListItem key={i} />
-      ))}
-      <CircleButton>+</CircleButton>
-    </View>
-  )
+  // リダイレクト設定
+  return <Redirect href="memo/list"></Redirect>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white'
-  }
-})
 
 export default Index
---
+++ src/app/memo/list.tsx
@@ -0,0 +1,30 @@
+import { View, StyleSheet } from 'react-native'
+import Header from '../../components/Header'
+import MemoListItem from '../../components/MemoListItem'
+import CircleButton from '../../components/CircleButton'
+
+const Index = (): JSX.Element => {
+  // 空オブジェクトのリストを作成
+  const list: object[] = []
+  for (let index = 0; index < 2; index++) {
+    list.push({})
+  }
+  return (
+    <View style={styles.container}>
+      <Header />
+      {list.map((_, i) => (
+        <MemoListItem key={i} />
+      ))}
+      <CircleButton>+</CircleButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  }
+})
+
+export default Index
