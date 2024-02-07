be77af0e9b7b406d9591f58d7bed7870d701fd97
tokushun109 <tokushun109@gmail.com>
2024-02-07 14:25:32 +0900

    headerのスタイルを追加

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,10 +8,10 @@ const Index = (): React.JSX.Element => {
   }
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
@@ -37,8 +37,29 @@ const Index = (): React.JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  header: {
+    backgroundColor: 'blue',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
+    position: 'relative',
     alignItems: 'center'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.7)'
   }
 })
 
