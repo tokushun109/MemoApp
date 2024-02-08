54af28838f9f72f609c2e4e3bd76b76b81b0ad58
tokushun109 <tokushun109@gmail.com>
2024-02-08 13:32:17 +0900

    userごとにメモを作成

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -3,18 +3,44 @@ import { IconEnum } from '../../../types/icon'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import { router } from 'expo-router'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { auth, db } from '../../config'
+import { useState } from 'react'
 
-const handlePress = (): void => {
-  router.back()
+const handlePress = async (bodyText: string): Promise<void> => {
+  try {
+    if (auth.currentUser === null) throw new Error('ログインされていません')
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    await addDoc(ref, {
+      bodyText,
+      updateAt: Timestamp.fromDate(new Date())
+    })
+    router.back()
+  } catch {}
 }
 
 const Create = (): JSX.Element => {
+  const [bodyText, setBodyText] = useState<string>('')
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="height">
       <View style={styles.inputContainer}>
-        <TextInput multiline style={styles.input} value="" />
+        <TextInput
+          multiline
+          style={styles.input}
+          value={bodyText}
+          onChangeText={(value) => {
+            setBodyText(value)
+          }}
+          autoCapitalize="none"
+          autoFocus
+        />
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton
+        onPress={() => {
+          void handlePress(bodyText)
+        }}
+      >
         <Icon name={IconEnum.Check} />
       </CircleButton>
     </KeyboardAvoidingView>
