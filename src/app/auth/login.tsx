06fae1602885fdcf53cfa95a61ed1181572ab077
tokushun109 <tokushun109@gmail.com>
2024-02-08 10:14:49 +0900

    useStateの追加

--- src/app/auth/login.tsx
+++ src/app/auth/login.tsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native'
 import { Link, router } from 'expo-router'
 import Button from '../../components/Button'
+import { useState } from 'react'
+import { type User } from '../../../types/user'
 
 const handlePress = (): void => {
   // TODO ログイン
@@ -8,12 +10,45 @@ const handlePress = (): void => {
 }
 
 const Login = (): JSX.Element => {
+  const [user, setUser] = useState<User>({ email: '', password: '' })
+
+  const onChangeText = (value: string, type: 'email' | 'password'): void => {
+    switch (type) {
+      case 'email':
+        setUser({ ...user, email: value })
+        break
+      case 'password':
+        setUser({ ...user, password: value })
+        break
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
-        <TextInput style={styles.input} value="Email" />
-        <TextInput style={styles.input} value="Pass" />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(value) => {
+            onChangeText(value, 'email')
+          }}
+          value={user.email}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={(value) => {
+            onChangeText(value, 'password')
+          }}
+          value={user.password}
+          secureTextEntry
+          autoCapitalize="none"
+          textContentType="password"
+        />
         <Button onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Not Registered?</Text>
